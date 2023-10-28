77037b3bc41c1ad9b5b1b63f766f3269a7bb6f29
LEON-BROTHER <64485923+LEON-BROTHER@users.noreply.github.com>
2023-10-28 20:11:59 +0200

    Create mine.lua

---
+++ mine.lua
@@ -0,0 +1,33 @@
+-- Start
+local distance = 0
+print("Wilkommen")
+print("Wie weit soll die Turtle minen?")
+distance = tonumber(io.input());
+
+for i=1,distance do
+    turtle.dig()
+    turtle.digDown()
+    turtle.forward()
+    if i % 5 == 0 then
+        turtle.turnLeft()
+        for j=1,10 do
+            turtle.dig()
+            turtle.forward()
+        end
+        for j=1,10 do
+            turtle.back()
+        end
+        turtle.turnLeft()
+        turtle.turnLeft()
+        for j=1,10 do
+            turtle.dig()
+            turtle.forward()
+        end
+        for j=1,10 do
+            turtle.back()
+        end
+        turtle.turnLeft()
+    end
+end
+
+
